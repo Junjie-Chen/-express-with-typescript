6b0da170e2fcb474099a4050ee16c21abca8bd3f
Junjie Chen <junjie.chen18@gmail.com>
2020-05-17 18:52:01 -0400

    feat(app): listen for connection

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['express with typescript'] }));
 app.use(router);
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000.');
+});
