b4581d22a6ba465363b2baef77abedcc207cbc3d
Junjie Chen <junjie.chen18@gmail.com>
2020-05-17 18:53:16 -0400

    feat(router): handle get request to /

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,3 +1,21 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
+
+router.get('/', (req: Request, res: Response): void => {
+  if (req.session && req.session.isLoggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in.</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in.</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
