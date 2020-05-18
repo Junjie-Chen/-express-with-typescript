c394ede91f55a2d88efe230d2a69404ba21548d6
Junjie Chen <junjie.chen18@gmail.com>
2020-05-17 21:15:11 -0400

    feat(validator): add desc parameter

--- src/controllers/decorators/validator.ts
+++ src/controllers/decorators/validator.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
+import { RequestHandlerDescriptor } from '../../interfaces/RequestHandlerDescriptor';
 import { Keys } from './Keys';
 
 export function validator(...properties: string[]): Function {
-  return function(target: any, key: string): void {
+  return function(target: any, key: string, desc: RequestHandlerDescriptor): void {
     Reflect.defineMetadata(Keys.Validator, properties, target, key);
   };
 }
