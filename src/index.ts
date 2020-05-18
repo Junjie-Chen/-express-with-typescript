10d48d3fd29cc382ffe134b504943eb2619875de
Junjie Chen <junjie.chen18@gmail.com>
2020-05-17 20:27:31 -0400

    feat(app): use Router middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { Router } from './Router';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['express with typescript'] }));
+app.use(Router.createRouter());
