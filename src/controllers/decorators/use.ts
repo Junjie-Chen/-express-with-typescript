5c838a64d2e07e4867993279aa61468a8bb92e0c
Junjie Chen <junjie.chen18@gmail.com>
2020-05-17 21:01:54 -0400

    feat(use): add desc parameter

--- src/controllers/decorators/use.ts
+++ src/controllers/decorators/use.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 import { RequestHandler } from 'express';
+import { RequestHandlerDescriptor } from '../../interfaces/RequestHandlerDescriptor';
 import { Keys } from './Keys';
 
 export function use(middleware: RequestHandler): Function {
-  return function(target: any, key: string): void {
+  return function(target: any, key: string, desc: RequestHandlerDescriptor): void {
     const middlewares = Reflect.getMetadata(Keys.Middleware, target, key) || [];
 
     Reflect.defineMetadata(Keys.Middleware, [...middlewares, middleware], target, key);
