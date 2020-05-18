bb6fdc383ca707da322fc760b70581beafdded8e
Junjie Chen <junjie.chen18@gmail.com>
2020-05-17 20:26:47 -0400

    feat(app): use cookieSession middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['express with typescript'] }));
