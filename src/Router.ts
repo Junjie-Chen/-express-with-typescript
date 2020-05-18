9311688842a7307b7b3b27789d3a10f6911a4e86
Junjie Chen <junjie.chen18@gmail.com>
2020-05-17 20:31:07 -0400

    feat(Router): define static createRouter method

--- src/Router.ts
+++ src/Router.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 export class Router {
   private static instance: express.Router;
+
+  static createRouter(): express.Router {
+    if (!Router.instance) {
+      Router.instance = express.Router();
+    }
+
+    return Router.instance;
+  }
 }
